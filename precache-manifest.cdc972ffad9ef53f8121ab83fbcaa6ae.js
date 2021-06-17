2a0eaa0efb30890386048f28cd53f7a1db084304
Oleksandr Vasilkov <vasilkoff1337@gmail.com>
2021-06-17 13:13:52 +0300

    Updates

---
+++ precache-manifest.cdc972ffad9ef53f8121ab83fbcaa6ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423d3811102a6a3cf6b7ae63ff2bc26d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb6fcc9255810c41fe87",
+    "url": "./static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "f70fa610ae01f6e744a6",
+    "url": "./static/js/2.b45151c1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b45151c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6fcc9255810c41fe87",
+    "url": "./static/js/main.509f5d67.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
